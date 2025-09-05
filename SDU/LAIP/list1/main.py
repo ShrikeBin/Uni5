fb44545578ab79d44a2f01d71c12a71d2c27a313
ShrikeBin <grahamka.ha@gmail.com>
2025-09-05 16:01:14 +0200

    update sep 5

--- SDU/LAIP/list1/main.py
+++ SDU/LAIP/list1/main.py
@@ -15,7 +15,7 @@ def task1():
     B = np.array([6300, 267.75])
     
     X = np.linalg.solve(A, B)
-    print(f"Task 1: {X[0]:.4f} in obligations and {X[1]:.4f} in money market fund")
+    print(f"Task 1:\n {X[0]:.4f} in obligations and {X[1]:.4f} in money market fund")
     
     
 # TASK 2
@@ -43,12 +43,11 @@ def task2():
                 list(p_3.values()),
                 list(p_4.values()),
                 list(p_5.values())])
-    print(A)
     # vector of quantities produced
     X = np.array([10,9,12,13,11])
     # total resources consumed by each resource
     B = X.T @ A 
-    print(f"Task 2: Total metal consumed:\n {B[0]:.4f} units")
+    print(f"Task 2:\n Total metal consumed: {B[0]:.4f} units")
 
 
 # TASK 3
@@ -68,17 +67,64 @@ def task3():
             list(p_4.values()),
             list(p_5.values())])
     
-    print("Task 3: Supposing now that I do not know how much of each product is produced but I know how much of each material is used: \n \
+    print("Task 3:\n Supposing now that I do not know how much of each product is produced but I know how much of each material is used: \n \
           1. Could you determine from this information the amount produced of each product? \n \
           2. Will a solution exist? \n \
           3. Will it be unique?")
-    print("If det(A) given A = matrix of p_i i = 1..n is not 0, then a unique solution exists.")
-    print("If det(A) = 0, then: \n \
+    print(" If det(A) given A = matrix of p_i i = 1..n is not 0, then a unique solution exists.")
+    print(" If det(A) = 0, then: \n \
           1. no solution - after reduction of lineary dependent rows, \n \
              the solution vector B isnt to be found in the new space spanned by the rows of A. \n \
           2. infinite solutions - after reduction of lineary dependent rows,\n \
              the solution vector B is to be found in the new space spanned by the rows of A. \n \
              Depending on the number k of lineary dependent rows, there will an k dimensional space of solutions.")
     print(f"det(A) = {np.linalg.det(A):.4f}")
+    
+    
+# TASK 4
 
-task3()
+# Brewery problem:
+# x1 - Dark beer
+# x2 - Ale
+
+# Linear problem:
+
+# max 23*x1 + 13*x2
+# 15*x1 + 5x2 <= 480
+# 4*x1 + 4*x2 <= 160
+# 20*x1 + 35*x2 <= 1190
+# x1, x2 >= 0
+
+def task4(wantupdated = 1):
+    # A = np.array([[15, 5],
+    #               [4, 4],
+    #               [20, 35]])
+    # b = np.array([480, 160, 1190]).T
+    # x1, x2 = 1
+    # x = np.array([x1, x2]).T
+    # maximize x so that Ax <= b
+    if not wantupdated:
+        print("Brewery problem:\n \
+                x1 - Dark beer\n \
+                x2 - Ale\n \
+                \n \
+                Linear problem: \n \
+                \n \
+                max 23*x1 + 13*x2\n \
+                15*x1 + 5x2 <= 480\n \
+                4*x1 + 4*x2 <= 160\n \
+                20*x1 + 35*x2 <= 1190\n \
+                x1, x2 >= 0")
+    else:    
+        print("Brewery updated problem:\n \
+            x1 - Dark beer\n \
+            x2 - Ale\n \
+            x3 - Light\n \
+            \n \
+            Linear problem: \n \
+            \n \
+            max 23*x1 + 13*x2 + 15*x3\n \
+            15*x1 + 5x2 + 10*x3 <= 480\n \
+            4*x1 + 4*x2 + 2*x3 <= 160\n \
+            20*x1 + 35*x2 + 15*x3 <= 1190\n \
+            x1, x2, x3 >= 0")
--- SDU/SM/list0/script.R
+++ SDU/SM/list0/script.R
@@ -1,3 +1,13 @@
 # script.R
-df <- read.csv("HearingThresholds.csv")
-print(head(df))
+data <- read.csv("HearingThresholds.csv", header = TRUE)
+print("data type: ")
+print(class(data))
+data$avg1k = (data$l1k + data$r1k)/2
+data$left = (data$l500 + data$l1k + data$l2k + data$l3k + data$l4k + data$l6k + data$l8k)/7
+data$right = (data$r500 + data$r1k + data$r2k + data$r3k + data$r4k + data$r6k + data$r8k)/7
+data$avg = (data$right + data$left)/2
+
+hist(data$avg, breaks = seq(from = 0, to = 90, by = 5),
+xlab = "Hearing threshold (dB)", ylab = "Frequency",
+main = "Distribution of hearing levels")
+
